0c860d11512770bde70e210c5e8fedec4cf54b3a
Scott McElroy <scmc9455@colorado.edu>
2018-03-04 19:12:23 -0500

    Stubs added to the UNIT_TESTS files

--- src/unit_tests.c
+++ src/unit_tests.c
@@ -23,15 +23,202 @@ circbuf.c
 Created for ECEN5813
 **********************************************************************************************/
 
-#include <stdio.h>
-#include <stdint.h>
-#include "platform.h"
-#include "project1.h"
-#include "memory.h"
-#include "conversion.h"
-#include "debug.h"
+#include "unit_tests.h"
 
-#include "project2.h"
-#include "uart.h"
-#include "circbuf.h"
-#include "gpio.h"
+/*********************************************************************************************/
+/***********************************main for testing******************************************/
+/**********************************************************************************************
+@brief- This function is the main loop for the tests
+
+@param - void
+@return - void
+**********************************************************************************************/
+
+void main()
+{
+   uint8_t total=0;   
+ 
+    printf("total = %d \n",total)
+    total += memmove_tests();
+    printf("total = %d \n",total);
+    total += memset_tests();
+    printf("total = %d \n",total)
+    total += memzero_tests();
+    printf("total = %d \n",total)
+    total += reverse_tests();
+    printf("total = %d \n",total)
+    total += atoi_tests();
+    printf("total = %d \n",total)
+    total += itoa_tests();
+    printf("total = %d \n",total)
+    total += endianness_tests();
+    printf("total = %d \n",total)
+    total += circbuf_tests();
+
+    printf("The total number of functions that failed is %d \n",total);
+
+    return;
+}
+
+/*********************************************************************************************/
+/**************************************memmove_tests******************************************/
+/**********************************************************************************************
+@brief- This function tests the memmove features
+
+Tests:
+invalid pointers
+No overlap
+SRC in DST
+DST in SRT
+DST == SRC
+
+@param - void
+@return - test_type: passing of failing
+**********************************************************************************************/
+
+test_type_e memmove_tests()
+{
+    return TEST_PASS;
+}
+
+/*********************************************************************************************/
+/**************************************memset_tests******************************************/
+/**********************************************************************************************
+@brief- This function tests the memmove features
+
+Tests:
+invalid pointers
+checks the set
+
+@param - void
+@return - test_type: passing of failing
+**********************************************************************************************/
+
+test_type_e memset_tests()
+{
+    return TEST_PASS;     
+}
+
+/*********************************************************************************************/
+/**************************************memzero_tests******************************************/
+/**********************************************************************************************
+@brief- This function tests the memzero features
+
+Tests:
+invalid pointers
+checks the set
+
+@param - void
+@return - test_type: passing of failing
+**********************************************************************************************/
+
+test_type_e memzero_tests()
+{
+    return TEST_PASS;    
+}
+
+/*********************************************************************************************/
+/**************************************reverse_tests******************************************/
+/**********************************************************************************************
+@brief- This function tests the reverse features
+
+Tests:
+invalid pointers
+checks odd reverses
+checks even reverses
+checks characters
+
+@param - void
+@return - test_type: passing of failing
+**********************************************************************************************/
+
+test_type_e reverse_tests()
+{
+    return TEST_PASS;    
+}
+
+/*********************************************************************************************/
+/*****************************************atoi_tests******************************************/
+/**********************************************************************************************
+@brief- This function tests the atoi function
+
+Tests:
+invalid pointers
+zero integers
+max sized integers
+
+@param - void
+@return - test_type: passing of failing
+**********************************************************************************************/
+
+test_type_e atoi_tests()
+{
+    return TEST_PASS;    
+}
+
+/*********************************************************************************************/
+/*****************************************itoa_tests******************************************/
+/**********************************************************************************************
+@brief- This function tests the itoa function
+
+Tests:
+invalid pointers
+zero integers
+max sized integers
+
+@param - void
+@return - test_type: passing of failing
+**********************************************************************************************/
+
+test_type_e itoa_tests()
+{
+    return TEST_PASS;    
+}
+
+/*********************************************************************************************/
+/***********************************endianness_tests******************************************/
+/**********************************************************************************************
+@brief- This function tests the endianness function
+
+Tests:
+invalid pointers
+valid conversions
+conversions back
+
+@param - void
+@return - test_type: passing of failing
+**********************************************************************************************/
+
+test_type_e endianness_tests()
+{
+    return TEST_PASS;    
+}
+
+/*********************************************************************************************/
+/*************************************circbuf_tests*******************************************/
+/**********************************************************************************************
+@brief- This function tests the circular buffer function
+
+Tests: 
+checks to make sure buffer is on the heap
+invalid pointers
+non-initialized buffers
+Add and remove features
+buffer full feature
+Wrap-add
+Wrap remove
+Overfill
+Over Empty
+
+@param - void
+@return - test_type: passing of failing
+**********************************************************************************************/
+
+test_type_e circbuf_tests()
+{
+    return TEST_PASS;    
+}
+
+/*********************************************************************************************/
+/*************************************circbuf_tests*******************************************/
+/*********************************************************************************************/
